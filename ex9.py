10b3b44cda1bc3b8705311bfd388746896907808
mcafigo <michael.c.alvaro@gmail.com>
2018-11-08 09:06:19 +1100

    Create ex9.py

---
+++ ex9.py
@@ -0,0 +1,20 @@
+# Here's some strange new stuff, remember to type it exactly
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# days creates the variable for days of the week
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+# months creates the variable for months listed within. The \n does a line break
+
+print("Here are the days: ", days)
+# lists out the items in variable days
+print("Here are the months: ", months)
+# lists out the items in variable months
+
+print("""
+There's something going on here.
+With the three double quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
+
+# prints out using triple quote which allows the string to keep on going
