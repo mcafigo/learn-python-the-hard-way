d9ef6646aa944220e754b83b553a313aea4cf033
Michael Alvaro <michael.c.alvaro@gmail.com>
2019-07-28 18:59:09 +1000

    new exercises

---
+++ ex11.py
@@ -0,0 +1,8 @@
+print("How old are you? (in years)", end=' ')
+age = input()
+print("How tall are you? (in centimetres)", end=' ')
+height = input()
+print("How much do you weigh? (in kg)", end=' ')
+weight = input()
+
+print(f"So, you're {age} years old, {height} centimetres tall and weigh {weight} kilograms.")
